1ad5d19f164874d3d792bfc4bfe9d95110c3ec23
Marc Tober <marc@tober-kerken.de>
2017-04-05 11:24:58 +0200

    GoLive 20170405-2

--- functions.sh
+++ functions.sh
@@ -472,7 +472,7 @@ log_info "$FUNCNAME $LINENO start $1 $2 $3"
 local -r i_url="$1"
 local -r i_idx_tmp="$2"
 
-# l_response=$(curl "https://roads.googleapis.com/v1/snapToRoads?path=-35.27801,149.12958|-35.28032,149.12907|-35.28099,149.12929|-35.28144,149.12984|-35.28194,149.13003|-35.28282,149.12956|-35.28302,149.12881|-35.28473,149.12836&interpolate=true&key=AIzaSyDsRlbhcPoQLhpSuYios3ircMSoeZP0f5Y")
+# l_response=$(curl "https://roads.googleapis.com/v1/snapToRoads?path=-35.27801,149.12958|-35.28032,149.12907|-35.28099,149.12929|-35.28144,149.12984|-35.28194,149.13003|-35.28282,149.12956|-35.28302,149.12881|-35.28473,149.12836&interpolate=true&key=AIz")
 
 l_response=$(cat maps_response.txt)
 
